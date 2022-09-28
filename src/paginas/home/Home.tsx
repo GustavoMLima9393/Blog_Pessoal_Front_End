e733eefeceb33ad0d00efe29105b5c60f6def49e
Gustavo <gustavomlima9393@gmail.com>
2022-09-28 18:19:06 -0300

    Estilizando o componente com CSS

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -5,7 +5,7 @@ function Home(){
     return(
         <>
             <h1>Home</h1>
-            <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial" width="500" height="500"/>
+            <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial" className="img"/>
         </>
     );
 }
