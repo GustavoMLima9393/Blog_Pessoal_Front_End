e4a71ea34d6c1dc2955f7b97c9a7f0e21a475ca5
Gustavo <gustavomlima9393@gmail.com>
2022-09-29 22:13:37 -0300

    Criando Navbar footer e refatorar

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,23 +1,29 @@
-import React from "react";
-import {Button, Paper} from "@material-ui/core";
-import {Box} from "@mui/material";
-import "./Home.css";
+import React from 'react';
+import {Typography, Grid, Button} from '@material-ui/core';
+import {Box} from '@mui/material';
+import './Home.css';
 
-function Home(){
-    return(
+function Home() {
+    return (
         <>
-            <Paper>
-                <Box p={2}>
-                    <Box display="flex" justifyContent="center">
-                        <h1>Título</h1>
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>Expresse aqui os seus pensamentos e opiniões!</Typography>
                     </Box>
-                        <img src="https://neilpatel.com/wp-content/uploads/2017/07/corporatepersonalblog.jpg" alt="Imagem provisória" style={{width: "100%", height: "100%"}}/>
-                        <Box display="flex" justifyContent="center" p={2}>
-                            <Button variant="contained" color="primary">Botão 1</Button>
-                            <Button variant="contained" color="secondary">Botão 2</Button>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
                         </Box>
-                </Box>
-            </Paper>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
         </>
     );
 }
