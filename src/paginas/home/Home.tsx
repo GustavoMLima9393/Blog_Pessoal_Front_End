f4a6259d518d3fb088ca42fa53d83a8786453765
Gustavo <gustavomlima9393@gmail.com>
2022-09-29 21:09:51 -0300

    Adicionando Grid e FlexBox

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import {Button, Paper} from "@material-ui/core";
+import {Box} from "@mui/material";
 import "./Home.css";
 
 function Home(){
     return(
         <>
-            <h1>Home</h1>
-            <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial" className="img"/>
+            <Paper>
+                <Box p={2}>
+                    <Box display="flex" justifyContent="center">
+                        <h1>Título</h1>
+                    </Box>
+                        <img src="https://neilpatel.com/wp-content/uploads/2017/07/corporatepersonalblog.jpg" alt="Imagem provisória" style={{width: "100%", height: "100%"}}/>
+                        <Box display="flex" justifyContent="center" p={2}>
+                            <Button variant="contained" color="primary">Botão 1</Button>
+                            <Button variant="contained" color="secondary">Botão 2</Button>
+                        </Box>
+                </Box>
+            </Paper>
         </>
     );
 }
