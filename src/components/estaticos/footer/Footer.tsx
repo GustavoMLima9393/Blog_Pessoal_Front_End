c1276202e5597e44529e5b69bc76dc85d77fe09f
Gustavo <gustavomlima9393@gmail.com>
2022-10-06 17:18:06 -0300

    Alterando cores de Footer e Navbar

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import {Typography, Grid} from '@material-ui/core';
 import {Box} from '@mui/material';
 import './Footer.css';
@@ -13,16 +12,13 @@ function Footer() {
                 <Grid alignItems="center" item xs={12}>
                     <Box className='box1'>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom className="textos">Siga-nos nas redes sociais </Typography>
+                            <Typography variant="h5" align="center" gutterBottom className="textos">Me siga no Github e no Linkedin! </Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://www.facebook.com/generationbrasil" target="_blank">
-                                <FacebookIcon className='redes' />
+                            <a href="https://github.com/gustavomlima9393" target="_blank">
+                                <GitHubIcon className='redes' />
                             </a>
-                            <a href="https://www.instagram.com/generationbrasil/" target="_blank">
-                                <InstagramIcon className='redes' />
-                            </a>
-                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
+                            <a href="https://www.linkedin.com/in/gustavo-macedo-de-lima/" target="_blank">
                                 <LinkedInIcon className='redes' />
                             </a>
                         </Box>
