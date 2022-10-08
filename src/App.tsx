0caa63a5f15af0ead38890039fdbcd7501478073
Gustavo <gustavomlima9393@gmail.com>
2022-10-08 11:29:18 -0300

    Adicionando CadastroTema DeletarTema DeletarPostagem

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,10 @@ import Login from "./paginas/login/Login";
 import "./App.css";
 import ListaTema from "./components/temas/listaTema/ListaTema";
 import ListaPostagem from "./components/postagens/listaPostagem/ListaPostagem";
+import CadastroPost from "./components/postagens/cadastroPost/CadastroPost";
+import DeletarTema from "./components/temas/deletarTema/DeletarTema";
+import CadastroTema from "./components/temas/cadastroTema/CadastroTema";
+import DeletarPostagem from "./components/postagens/deletarPostagem/DeletarPostagem";
 
 function App() {
   return (
@@ -20,6 +24,12 @@ function App() {
         <Route path="/cadastrousuario" element={<CadastroUsuario />} />
         <Route path="/temas" element={<ListaTema />} />
         <Route path="/posts" element={<ListaPostagem />} />
+        <Route path="/formularioPostagem" element={<CadastroPost />} />
+        <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+        <Route path="/formularioTema" element={<CadastroTema />} />
+        <Route path="/formularioTema/:id" element={<CadastroTema />} />
+        <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+        <Route path="/deletarTema/:id" element={<DeletarTema />} />
       </Routes>
       <Footer />
     </BrowserRouter>
