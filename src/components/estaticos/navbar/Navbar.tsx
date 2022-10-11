7763063e2d97815355e5d906312f9a78dd56d702
Gustavo <gustavomlima9393@gmail.com>
2022-10-11 13:42:55 -0300

    Autenticação de token e renderização de condicional

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -5,68 +5,78 @@ import { AppBar, Toolbar, IconButton, Typography, Button } from "@material-ui/co
 import { Box } from '@mui/material';
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import useLocalStorage from "react-use-localstorage";
 import './Navbar.css';
 import { color } from "@mui/system";
+import { useDispatch, useSelector } from "react-redux";
+import { TokenState } from "../../../store/tokens/tokensReducer";
+import { addToken } from "../../../store/tokens/actions";
 
 function Navbar() {
-  const [token, setToken] = useLocalStorage('token')
+  const token = useSelector<TokenState, TokenState["tokens"]>(
+    (state) => state.tokens
+  );
   let navigate = useNavigate();
+  const dispatch = useDispatch();
 
   function goLogout() {
-    setToken('')
+    dispatch(addToken(""));
     alert("Usuário deslogado")
     navigate('/login')
   }
-  return (
-    <>
-      <AppBar position="static" style={{ backgroundColor: '#0a0124' }}>
-        <Toolbar variant="dense">
-          <Box className="cursor">
-            <Typography variant="h5" color="inherit">
-              Blog do Gustavo
-            </Typography>
-          </Box>
 
-          <Box display="flex" justifyContent="start">
-            <Link to="/home" className="text-decorator-none">
-              <Box mx={2} className="cursor">
-                <Typography variant="h6" color="inherit">
-                  Home
-                </Typography>
-              </Box>
-            </Link>
-            <Link to="/posts" className="text-decorator-none">
-              <Box mx={1} className="cursor">
-                <Typography variant="h6" color="inherit">
-                  Postagens
-                </Typography>
-              </Box>
-            </Link>
-            <Link to="/temas" className="text-decorator-none">
-              <Box mx={1} className="cursor">
-                <Typography variant="h6" color="inherit">
-                  Temas
-                </Typography>
-              </Box>
-            </Link>
-            <Link to="/formulariotema" className="text-decorator-none">
-              <Box mx={1} className="cursor">
-                <Typography variant="h6" color="inherit">
-                  Cadastrar Tema
-                </Typography>
-              </Box>
-            </Link>
+  var navbarComponent;
 
-            <Box mx={1} className="cursor" onClick={goLogout}>
+  if (token !== "") {
+    navbarComponent = <AppBar position="static" style={{ backgroundColor: '#0a0124' }}>
+      <Toolbar variant="dense">
+        <Box className="cursor">
+          <Typography variant="h5" color="inherit">
+            Blog do Gustavo
+          </Typography>
+        </Box>
+
+        <Box display="flex" justifyContent="start">
+          <Link to="/home" className="text-decorator-none">
+            <Box mx={2} className="cursor">
               <Typography variant="h6" color="inherit">
-                Logout
+                Home
               </Typography>
             </Box>
+          </Link>
+          <Link to="/posts" className="text-decorator-none">
+            <Box mx={1} className="cursor">
+              <Typography variant="h6" color="inherit">
+                Postagens
+              </Typography>
+            </Box>
+          </Link>
+          <Link to="/temas" className="text-decorator-none">
+            <Box mx={1} className="cursor">
+              <Typography variant="h6" color="inherit">
+                Temas
+              </Typography>
+            </Box>
+          </Link>
+          <Link to="/formulariotema" className="text-decorator-none">
+            <Box mx={1} className="cursor">
+              <Typography variant="h6" color="inherit">
+                Cadastrar Tema
+              </Typography>
+            </Box>
+          </Link>
 
+          <Box mx={1} className="cursor" onClick={goLogout}>
+            <Typography variant="h6" color="inherit">
+              Logout
+            </Typography>
           </Box>
-        </Toolbar>
-      </AppBar>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  }
+  return (
+    <>
+      {navbarComponent}
     </>
   );
 }
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -4,13 +4,16 @@ import { Box } from '@mui/material';
 import './Home.css';
 import TabPostagem from '../../components/postagens/tabPostagem/TabPostagem';
 import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
-import useLocalStorage from 'react-use-localstorage';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../store/tokens/tokensReducer';
 
 function Home() {
 
     let navigate = useNavigate();
-    const [token, setToken] = useLocalStorage('token');
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
 
     useEffect(() => {
         if (token == "") {
@@ -32,7 +35,9 @@ function Home() {
                         <Box marginRight={1}>
                             <ModalPostagem />
                         </Box>
-                        <Button type="submit" variant="contained" color="primary">Ver Postagens</Button>
+                        <Link to="/posts" className="text-decorator-none">
+                            <Button type="submit" variant="contained" color="primary">Ver Postagens</Button>
+                        </Link>
                     </Box>
                 </Grid>
                 <Grid item xs={6} className="imagem1">
