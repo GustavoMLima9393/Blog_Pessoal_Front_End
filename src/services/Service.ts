9ff7f5598c1c7930f6b27be65b796547d0ad5298
Gustavo <gustavomlima9393@gmail.com>
2022-10-11 15:07:50 -0300

    Retornando URL do blog pessoal

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://blogpessoalbackendgen.herokuapp.com/"
+    baseURL: "https://blogpessoal-nkth.onrender.com"
 })
 
 export const cadastroUsuario = async (url: any, dados: any, setDado: any) => {
